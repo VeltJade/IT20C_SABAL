97b2e606dd3cdac3b87eaf0f7df32ebd4f10233b
VeltJade <judeyasabal@gmail.com>
2024-09-23 22:21:08 +0800

    first commit

---
+++ src/it20c_sabal/IT20C_Sabal.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package it20c_sabal;
+
+/**
+ *
+ * @author acer
+ */
+public class IT20C_Sabal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
