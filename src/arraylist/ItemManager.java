e6a79185248ff34827ab17e3e4c30e7c917c76f5
VeltJade <judeyasabal@gmail.com>
2024-09-29 17:00:30 +0800

    arraylist

---
+++ src/arraylist/ItemManager.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package arraylist;
+
+/**
+ *
+ * @author acer
+ */
+public class ItemManager {
+    public static void main(String[] args) {
+        Main Obj = new Main();
+        System.out.print(Obj);
+    }
+}
---
+++ src/arraylist/Main.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+package arraylist;
+
+import java.util.ArrayList;
+/**
+ *
+ * @author acer
+ */
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList();
+        names.add("Velt");
+        names.add("Jade");
+        names.add("Saliz");
+        names.add("Jhandie");
+        System.out.print(names);
+    }
+
+    static void get(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
